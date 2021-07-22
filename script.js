d13d1b2fba98e30c5f73fe9b0985a52558722bdf
Sara <degrangesara@gmail.com>
2021-07-22 09:10:28 +0200

    créations des fonctions de calculs

---
+++ script.js
@@ -0,0 +1,24 @@
+//------------ FONCTIONS ------------
+function additon(nombreA, nombreB) {
+    let resultat = nombreA + nombreB;
+    return resultat;
+}
+
+function soustraction(nombreA, nombreB) {
+    let resultat = nombreA - nombreB;
+    return resultat;
+}
+
+function multiplucation(nombreA, nombreB) {
+    let resultat = nombreA * nombreB;
+    return resultat;
+}
+
+function division(nombreA, nombreB) {
+    // création d'une erreur si nombre B vaut 0 
+    // if (nombreB===0) {
+        
+    // }
+    let resultat = nombreA / nombreB;
+    return resultat;
+}
