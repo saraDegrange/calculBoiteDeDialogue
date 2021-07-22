763ba5ce13c1dd328334b2125c0bcda9a45ca291
Sara <degrangesara@gmail.com>
2021-07-22 09:27:50 +0200

    mise en place du menu et du choix utilisateur

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+//------------ MAIN ------------
+
+let choix = "";
+let menu = "Que souhaitez vous faire ? \n";
+menu += "   1. Addition \n";
+menu += "   2. Soustraction \n";
+menu += "   3. Multiplication \n";
+menu += "   4. Division";
+
+do {
+ 
+    choix = Number(prompt(menu));
+
+} while ((choix != 1) && (choix != 2) && (choix != 3) && (choix != 4));
+
 //------------ FONCTIONS ------------
 function additon(nombreA, nombreB) {
     let resultat = nombreA + nombreB;
