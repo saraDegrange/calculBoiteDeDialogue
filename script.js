ea82e69febae4718da79c4cd84145abc2291c5fa
Sara <degrangesara@gmail.com>
2021-07-31 10:35:59 +0200

    ajout de la fonction recommencer

--- script.js
+++ script.js
@@ -9,41 +9,45 @@ menu += "   4. Division";
 let nb1, nb2;
 let resultat;
 let signe;
+let recommencer = false;
 
-do {
+do {    
+    do {
 
-    choix = Number(prompt(menu));
+        choix = Number(prompt(menu));
 
-} while ((choix != 1) && (choix != 2) && (choix != 3) && (choix != 4));
+    } while ((choix != 1) && (choix != 2) && (choix != 3) && (choix != 4));
 
-do {
-    nb1 = Number(prompt("Entrez le nombre 1 : "));
-    nb2 = Number(prompt("Entrez le nombre 2 : "));
+    do {
+        nb1 = Number(prompt("Entrez le nombre 1 : "));
+        nb2 = Number(prompt("Entrez le nombre 2 : "));
 
-} while (isNaN(nb1) || isNaN(nb2));
+    } while (isNaN(nb1) || isNaN(nb2));
 
-switch (choix) {
-    case 1:
-        resultat = additon(nb1, nb2);
-        signe = " + ";
-        break;
-    case 2:
-        resultat = soustraction(nb1, nb2);
-        signe = " - ";
-        break;
-    case 3:
-        resultat = multiplucation(nb1, nb2);
-        signe = " * ";
-        break;
-    case 4:
-        resultat = division(nb1, nb2);
-        signe = " / ";
-        break;
-    default:
-        break;
-}
+    switch (choix) {
+        case 1:
+            resultat = additon(nb1, nb2);
+            signe = " + ";
+            break;
+        case 2:
+            resultat = soustraction(nb1, nb2);
+            signe = " - ";
+            break;
+        case 3:
+            resultat = multiplucation(nb1, nb2);
+            signe = " * ";
+            break;
+        case 4:
+            resultat = division(nb1, nb2);
+            signe = " / ";
+            break;
+        default:
+            break;
+    }
 
-alert("Le resultat de " + nb1 + signe + nb2 + " est de " + resultat + ".");
+    alert("Le resultat de " + nb1 + signe + nb2 + " est de " + resultat + ".");
+    recommencer = confirm("Voulez vous refaire un calcul ? (Annuler pour quitter // ok pour recommencer) S");
+}while (recommencer === true);
 
 //------------ FONCTIONS ------------
 function additon(nombreA, nombreB) {
